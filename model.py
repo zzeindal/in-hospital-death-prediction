e081007ecadddca626511a94254c271500d80dd2
Aram Sargsian <aramsargsian@Arams-MacBook-Pro.local>
2024-02-04 18:13:59 +0400

    Hi guys

--- model.py
+++ model.py
@@ -3,18 +3,7 @@ from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_sc
 
 
 class Model:
-    """
-        A class for creating and evaluating a Linear Discriminant Analysis model.
 
-        Attributes:
-            lda_model (LinearDiscriminantAnalysis): The Linear Discriminant Analysis model.
-
-        Methods:
-            fit(X_train, y_train): Fit the model on the training data.
-            predict(X_test): Make predictions on new data.
-            predict_proba(X_test): Get class probabilities for predictions.
-            score(X_test, y_test): Evaluate the model's performance using F1 score, accuracy, recall, and precision.
-    """
 
     def __init__(self):
         """
